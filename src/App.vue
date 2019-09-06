194918455e3681dae393f196e44651d167e669e6
Martin <martin@moid.se>
2019-09-06 19:42:32 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <CameraImage/>
+  </div>
+</template>
+
+<script>
+import CameraImage from './components/CameraImage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CameraImage
+  }
+}
+</script>
---
+++ src/components/CameraImage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Camera image goes here =)
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CameraImage'
+}
+</script>
